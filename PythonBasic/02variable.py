e97b5dc54b48df4a84d9db4015090196e7f2774f
Ruchikaupuldeniya <ruchikaupuldeniya14@gmail.com>
2026-06-09 22:59:16 +0530

    Add files via upload

---
+++ PythonBasic/02variable.py
@@ -0,0 +1,14 @@
+student_count=1000
+print (student_count)
+
+rating=4.99   #this is float
+
+is_published=True   #this is boolean
+
+course_name="Program with python"   #this is string
+
+print(rating,is_published,course_name) 
+
+
+
+
---
+++ PythonBasic/03string.py
@@ -0,0 +1,16 @@
+msg = "Python Programming"
+ 
+print(len(msg))      #getting the length of the msg variable
+print(msg[0])        #get the first later of the msg 
+print(msg[0:5])      #get the latters index 0 to 5
+
+print(msg[0:8])      
+print(msg[0:-3])
+print(msg[:5])
+print(msg[5:])
+print(msg[:7])
+
+ 
+ 
+ 
+ 
